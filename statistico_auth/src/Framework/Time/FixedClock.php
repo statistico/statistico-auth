9469d3c57ada69d1794e06985ae66d856752e22c
Joe Sweeny <joesweeny84@hotmail.com>
2020-01-06 17:33:22 +0000

    Add clock classes

---
+++ statistico_auth/src/Framework/Time/FixedClock.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Opia\Lib\Time;
+
+use Cake\Chronos\Chronos;
+use DateTimeImmutable;
+
+class FixedClock implements Clock
+{
+    /**
+     * @var DateTimeImmutable
+     */
+    private $currentDatetime;
+
+    public function __construct(DateTimeImmutable $currentDatetime = null)
+    {
+        $this->currentDatetime = $currentDatetime ?: new DateTimeImmutable();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getCurrentDatetime()
+    {
+        return Chronos::instance($this->currentDatetime);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function now()
+    {
+        return $this->getCurrentDatetime();
+    }
+
+    /**
+     * Travel to the given datetime.
+     *
+     * @param DateTimeImmutable $time
+     */
+    public function travelTo(DateTimeImmutable $time)
+    {
+        $this->currentDatetime = $time;
+    }
+
+    public function getCurrentUnixtime(): float
+    {
+        return $this->currentDatetime->getTimestamp();
+    }
+}
